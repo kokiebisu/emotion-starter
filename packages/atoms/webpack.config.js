127f016664c24a14df4a4e5705ebdf4ba09af2a2
Kenichi Okiebisu <a01056715@gmail.com>
2021-02-14 12:05:11 -0800

    feat: globalObject

--- packages/atoms/webpack.config.js
+++ packages/atoms/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     library: "emotion-starter-template",
     libraryTarget: "umd",
+    globalObject: 'this'
   },
   module: {
     rules: [
